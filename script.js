f0dcb7916cdddf92a42a74fe45b83923209f7e96
chai-py <er.chaital123@gmail.com>
2024-11-06 09:55:46 -0700

    added first draft of code for toast_activity

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
+//Toggles
+let isTimerDone = false; //istimerdone
+
+let isBreadInToaster = false; //isBreadinToaster
+
+let isToast = false;
+
 //bread variable
-
-//istimerdone
-
-//isBreadinToaster 
+const bread = {
+    size : [60, 40, 5],
+    burnPoint : 600 ,
+    toastPoint : 300 ,
+    currentTemp : 20 ,
+};
 
 //toaster variable
+const toaster = {
+    slots: 4,
+    button: (e) => {
+        if (e) {
+        return true;
+    }
+    return false;
+},
+};
 
 //Functions
 
 //cagedown
-
-//cageup
+function cageDown(e) {
+    if (e) {
+        //turn on heat
+        //start timer
+    }
+}
+//cageup TODO:
+function cageUP(e) {}
 
 //timer
+function setTimer(e, time = 120) {
+    if (e) {
+        //run activate heat
+        //Start Timer at length of time argument
+    }
+    //when timer done signal cageup // toggle cage && disable heat element
+}
 
 //activateHeat
+function activateHeat() {
+    //turn on heat element
+    //increase bread temp
+}
 
-//RemoveToastFromToaster
 
